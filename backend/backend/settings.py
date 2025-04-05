5796c7f237b35b402592887b7c050f3f2dc5795d
yoyoisaman <yoyoisaman@gmail.com>
2025-04-05 13:23:53 +0800

    add docker file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,6 +119,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ALLOW_ALL_ORIGINS = True
+ALLOWED_HOSTS = ['*']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
