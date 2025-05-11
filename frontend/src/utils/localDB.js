e1a753dd44d458ebcbd7b8f51d51197c7582b0e1
SniperTNT <wilson00gundam@gmail.com>
2025-05-11 23:48:46 +0800

    bugfix: localDB lastUpdate issue
    directly clear all current indexeddb table and use server data init table content

--- frontend/src/utils/localDB.js
+++ frontend/src/utils/localDB.js
@@ -51,15 +51,15 @@ await $.ajax({
 const databaseStatusTableName = "databaseStatus";
 const treeStructureTableName = "treeStructure";
 const bookmarksTableName = "bookmarks";
-let loaclDB = null;
+let localDB = null;
 
-const loaclDBPromise = new Promise((resolve, reject) => {
+const localDBPromise = new Promise((resolve, reject) => {
     let dbRequest = indexedDB.open("bookmarksDB", 2);
     dbRequest.onupgradeneeded = function (event) {
-        loaclDB = dbRequest.result;
+        localDB = dbRequest.result;
         if (event.oldVersion === 0) {  // local database initialization from server data
-            loaclDB.createObjectStore(treeStructureTableName);
-            loaclDB.createObjectStore(bookmarksTableName);
+            localDB.createObjectStore(treeStructureTableName);
+            localDB.createObjectStore(bookmarksTableName);
 
             let serverKeys = Object.keys(idToBookmark);
             serverKeys = serverKeys.map((key) => parseInt(key));
@@ -75,154 +75,101 @@ const loaclDBPromise = new Promise((resolve, reject) => {
             }
         }
         if (event.oldVersion <= 1) {  // ver1 -> ver2: record last_updated
-            loaclDB.createObjectStore(databaseStatusTableName);
+            localDB.createObjectStore(databaseStatusTableName);
             let databaseStatusStore = event.target.transaction.objectStore(databaseStatusTableName);
             databaseStatusStore.put(databaseStatus, 0);
         }
     };
     dbRequest.onsuccess = function (event) {
-        loaclDB = event.target.result;
+        localDB = event.target.result;
 
-        function getDataPromise(tableName) {
-            // returns a promise to get data from indexedDB
-            // when the promise is resolved, data is returned as an object with key as id and value as data
+        function clearTablePromise(tableName) {
+            // returns a promise to delete table from indexedDB
             return new Promise((resolve, reject) => {
-                let data = {};
-                let cursorRequest = loaclDB.transaction(tableName, "readonly")
-                    .objectStore(tableName)
-                    .openCursor();
-                cursorRequest.onsuccess = function (event) {
-                    let cursor = event.target.result;
-                    if (cursor) {
-                        data[cursor.key] = cursor.value;
-                        cursor.continue();
-                    } else {
-                        resolve(data);
-                    }
+                let transaction = localDB.transaction(tableName, "readwrite");
+                let store = transaction.objectStore(tableName);
+                let request = store.clear();
+                request.onsuccess = function (event) {
+                    resolve(event);
+                }
+                request.onerror = function (event) {
+                    reject(event);
                 }
             });
         }
 
-        function getDataUpdatePromises(localLastUpdated, serverLastUpdated) {
-            // returns an array of promises to update localDB with server data
-            // when all promises are resolved, update process is done
-            // TODO: update server data
-            if (localLastUpdated >= serverLastUpdated) {
-                return [Promise.resolve({})];
-            } else if (localLastUpdated < serverLastUpdated) {
-                let promises = [];
-                let serverKeys = Object.keys(idToBookmark);
-                serverKeys = serverKeys.map((key) => parseInt(key));
-                let databaseStatusStore = loaclDB.transaction(databaseStatusTableName, "readwrite").objectStore(databaseStatusTableName);
-                let treeStructureStore = loaclDB.transaction(treeStructureTableName, "readwrite").objectStore(treeStructureTableName);
-                let bookmarksStore = loaclDB.transaction(bookmarksTableName, "readwrite").objectStore(bookmarksTableName);
-
-                promises.push(new Promise((resolve, reject) => {
-                    let request = databaseStatusStore.put(databaseStatus, 0);
+        function updateTablePromises() {
+            let promises = [];
+            let serverKeys = Object.keys(idToBookmark);
+            serverKeys = serverKeys.map((key) => parseInt(key));
+            let treeStructureStore = localDB.transaction(treeStructureTableName, "readwrite").objectStore(treeStructureTableName);
+            let bookmarksStore = localDB.transaction(bookmarksTableName, "readwrite").objectStore(bookmarksTableName);
+            let databaseStatusStore = localDB.transaction(databaseStatusTableName, "readwrite").objectStore(databaseStatusTableName);
+            for (let i = 0; i < serverKeys.length; i++) {
+                let key = serverKeys[i];
+                let treeStructurePromise = new Promise((resolve, reject) => {
+                    let request = treeStructureStore.put(treeStructure[key], idToBookmark[key].id);
                     request.onsuccess = function (event) {
-                        resolve(event.target.result);
-                    };
-                }));
-                for (let i = 0; i < serverKeys.length; i++) {
-                    let key = serverKeys[i];
-                    promises.push(new Promise((resolve, reject) => {
-                        let request = treeStructureStore.put(treeStructure[key], idToBookmark[key].id);
-                        request.onsuccess = function (event) {
-                            resolve(event.target.result);
-                        };
-                    }));
-                }
-                for (let i = 0; i < serverKeys.length; i++) {
-                    let key = serverKeys[i];
-                    promises.push(new Promise((resolve, reject) => {
-                        let request = bookmarksStore.put(idToBookmark[key], idToBookmark[key].id);
-                        request.onsuccess = function (event) {
-                            resolve(event.target.result);
-                        };
-                    }));
-                }
-
-                // delete non-existing data in server
-                promises.push(new Promise((resolve, reject) => {
-                    treeStructureStore.openCursor().onsuccess = function (event) {
-                        let cursor = event.target.result;
-                        if (!cursor) {
-                            resolve();
-                            return;
-                        }
-
-                        if (!serverKeys.includes(cursor.key)) {
-                            let request = treeStructureStore.delete(cursor.key);
-                            promises.push(new Promise((resolve, reject) => {
-                                request.onsuccess = function (event) {
-                                    resolve(event.target.result);
-                                }
-                            }));
-                        }
-                        cursor.continue();
-                }}));
-                promises.push(new Promise((resolve, reject) => {
-                    bookmarksStore.openCursor().onsuccess = function (event) {
-                        let cursor = event.target.result;
-                        if (!cursor) {
-                            resolve();
-                            return;
-                        }
-
-                        if (!serverKeys.includes(cursor.key)) {
-                            let request = bookmarksStore.delete(cursor.key);
-                            promises.push(new Promise((resolve, reject) => {
-                                request.onsuccess = function (event) {
-                                    resolve(event.target.result);
-                                }
-                            }));
-                        }
-                        cursor.continue();
-                }}));
-
-                return promises;
+                        resolve(event);
+                    }
+                    request.onerror = function (event) {
+                        reject(event);
+                    }
+                });
+                promises.push(treeStructurePromise);
             }
+
+            for (let i = 0; i < serverKeys.length; i++) {
+                let key = serverKeys[i];
+                let bookmarksPromise = new Promise((resolve, reject) => {
+                    let request = bookmarksStore.put(idToBookmark[key], idToBookmark[key].id);
+                    request.onsuccess = function (event) {
+                        resolve(event);
+                    }
+                    request.onerror = function (event) {
+                        reject(event);
+                    }
+                });
+                promises.push(bookmarksPromise);
+            }
+
+            let databaseStatusPromise = new Promise((resolve, reject) => {
+                let request = databaseStatusStore.put(databaseStatus, 0);
+                request.onsuccess = function (event) {
+                    resolve(event);
+                }
+                request.onerror = function (event) {
+                    reject(event);
+                }
+            });
+            promises.push(databaseStatusPromise);
+
+            return promises;
         }
 
-        // get local database status (databaseStatusPromise)
-        // -> line 117 all update process is done
-        // -> line 120 get local treeStructure and bookmarks (getDataPromise)
-        // -> ine 125 resolve the outer promise (line 20 loaclDBPromise) with treeStructure and bookmarks
-        let databaseStatusPromise = getDataPromise(databaseStatusTableName);
-        databaseStatusPromise
-            .then((loaclDatabaseStatus) => {
-                let localLastUpdated = new Date(loaclDatabaseStatus[0].lastUpdated);
-                let serverLastUpdated = new Date(databaseStatus.lastUpdated);
-                let promises = getDataUpdatePromises(localLastUpdated, serverLastUpdated);
-                return Promise.all(promises);
-            })
-            .then(() => {
-                return Promise.all([
-                    getDataPromise(treeStructureTableName),
-                    getDataPromise(bookmarksTableName)
-                ]);
-            })
-            .then(([treeStructure, bookmarks]) => {resolve({treeStructure, bookmarks})})
-            .catch(reject);
+        Promise.all([
+            clearTablePromise(treeStructureTableName),
+            clearTablePromise(bookmarksTableName),
+            clearTablePromise(databaseStatusTableName),
+        ]).then(() => {
+            let promises = updateTablePromises();
+            return Promise.all(promises);
+        }).then(() => {
+            resolve();
+        }).catch(reject);
     };
     dbRequest.onerror = function (event) {
         throw new Error("loaclDB Error", event.target.error);
     };
 });
 
-// ensure loaclDBPromise is resolved -> loaclDB also initialized
-let loaclTreeStructure = null;
-let loaclBookmarks = null;
-await loaclDBPromise.then((data) => {
-    loaclTreeStructure = data.treeStructure;
-    loaclBookmarks = data.bookmarks;
-}).catch((error) => {
+await localDBPromise.catch((error) => {
     console.error("Error initializing localDB:", error);
 });
 
-function updateLoaclStatus(updateTime) {
+function updateLocalStatus(updateTime) {
     // update lastUpdated time in localDB
-    let transaction = loaclDB.transaction(databaseStatusTableName, "readwrite");
+    let transaction = localDB.transaction(databaseStatusTableName, "readwrite");
     let store = transaction.objectStore(databaseStatusTableName);
     let request = store.put({'lastUpdated': updateTime}, 0);
     request.onerror = function (event) {
@@ -234,7 +181,7 @@ const localDBfunc = {
     updateTreeStructure(id, data) {  // only for update existing treeStructure
         let updateTime = (new Date()).toISOString();
 
-        let transaction = loaclDB.transaction(treeStructureTableName, "readwrite");
+        let transaction = localDB.transaction(treeStructureTableName, "readwrite");
         let store = transaction.objectStore(treeStructureTableName);
         let checkRequest = store.get(id);
         checkRequest.onsuccess = function (event) {
@@ -252,7 +199,7 @@ const localDBfunc = {
             }
         }
 
-        updateLoaclStatus(updateTime);
+        updateLocalStatus(updateTime);
 
         $.ajax({
             url: 'http://localhost:8000/api/bookmarks/update/' + id,
@@ -278,7 +225,7 @@ const localDBfunc = {
     updateBookmark(id, data) {  // only for update existing bookmark or folder
         let updateTime = (new Date()).toISOString();
 
-        let transaction = loaclDB.transaction(bookmarksTableName, "readwrite");
+        let transaction = localDB.transaction(bookmarksTableName, "readwrite");
         let store = transaction.objectStore(bookmarksTableName);
         let checkRequest = store.get(id);
         checkRequest.onsuccess = function (event) {
@@ -300,7 +247,7 @@ const localDBfunc = {
             throw new Error("loaclDB Error", event.target.error);
         }
 
-        updateLoaclStatus(updateTime);
+        updateLocalStatus(updateTime);
 
         $.ajax({
             url: 'http://localhost:8000/api/bookmarks/update/' + id,
@@ -332,21 +279,21 @@ const localDBfunc = {
         // if existing id is passed, it will be updated instead of created
         let updateTime = (new Date()).toISOString();
 
-        let transaction = loaclDB.transaction(bookmarksTableName, "readwrite");
+        let transaction = localDB.transaction(bookmarksTableName, "readwrite");
         let store = transaction.objectStore(bookmarksTableName);
         let request = store.put(bookmarkData, id);
         request.onerror = function (event) {
             throw new Error("loaclDB Error", event.target.error);
         };
 
-        transaction = loaclDB.transaction(treeStructureTableName, "readwrite");
+        transaction = localDB.transaction(treeStructureTableName, "readwrite");
         store = transaction.objectStore(treeStructureTableName);
         request = store.put(treeStructureData, id);
         request.onerror = function (event) {
             throw new Error("loaclDB Error", event.target.error);
         };
 
-        updateLoaclStatus(updateTime);
+        updateLocalStatus(updateTime);
 
         $.ajax({
             url: 'http://localhost:8000/api/bookmarks/update/' + id,
@@ -378,21 +325,21 @@ const localDBfunc = {
     delId(id) {
         let updateTime = (new Date()).toISOString();
 
-        let transaction = loaclDB.transaction(treeStructureTableName, "readwrite");
+        let transaction = localDB.transaction(treeStructureTableName, "readwrite");
         let store = transaction.objectStore(treeStructureTableName);
         let request = store.delete(id);
         request.onerror = function (event) {
             throw new Error("loaclDB Error", event.target.error);
         };
 
-        transaction = loaclDB.transaction(bookmarksTableName, "readwrite");
+        transaction = localDB.transaction(bookmarksTableName, "readwrite");
         store = transaction.objectStore(bookmarksTableName);
         request = store.delete(id);
         request.onerror = function (event) {
             throw new Error("loaclDB Error", event.target.error);
         };
 
-        updateLoaclStatus(updateTime);
+        updateLocalStatus(updateTime);
 
         $.ajax({
             url: 'http://localhost:8000/api/bookmarks/delete/' + id,
@@ -415,4 +362,4 @@ const localDBfunc = {
     }
 }
 
-export { username as username, loaclTreeStructure as treeStructure, loaclBookmarks as idToBookmark, localDBfunc as localDB };
+export { username as username, treeStructure, idToBookmark, localDBfunc as localDB };
